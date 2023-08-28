055230a46493cfa42f64d2eb672801a04d384f88
Adham-Niazy <adhamniazy@gmail.com>
2023-08-28 19:07:57 +0300

    image upload utilite

---
+++ utils/imageUploading.ts
@@ -0,0 +1,17 @@
+import { serverUrl } from "@/constants/globals";
+
+const uploadImage = async (imagePath: string) => {
+  try {
+    const response = await fetch(`${serverUrl}/api/upload`, {
+      method: "POST",
+      body: JSON.stringify({
+        path: imagePath,
+      }),
+    });
+    return response.json();
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default uploadImage;
