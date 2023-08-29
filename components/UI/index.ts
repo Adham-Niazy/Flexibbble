0a9587ae7a9e96287ed1c688e1118f02c6ef459a
Adham-Niazy <adhamniazy@gmail.com>
2023-08-29 18:32:54 +0300

    Implement Categories component

--- components/UI/index.ts
+++ components/UI/index.ts
@@ -2,6 +2,7 @@ export { default as Button } from './Button';
 export { default as ProfileMenu } from './ProfileMenu';
 export { default as Modal } from './Modal';
 export { default as CustomMenu } from './CustomMenu';
+export { default as Categories } from './Categories';
 export { default as ProjectCard } from './ProjectCard';
 export { default as RelatedProjects } from './RelatedProjects';
 export { default as ProjectActions } from './ProjectActions';
